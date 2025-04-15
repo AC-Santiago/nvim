9345d9575bd79c3899dd5dcf87a9639e9989fa85
AC-Santiago <acostasantiago311124578@gmail.com>
2025-04-15 15:25:15 -0500

    Refactor: Add keymaps to use rust tools
    Implement the keymap to rust

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,23 +1,14 @@
 -- Keymaps específicos para archivos Rust
 local bufnr = vim.api.nvim_get_current_buf()
 
--- Acciones de código y hover
-vim.keymap.set("n", "<leader>ca", function()
-    vim.cmd.RustLsp("codeAction")
-end, { silent = true, buffer = bufnr, desc = "Code actions" })
-
 vim.keymap.set("n", "K", function()
     vim.cmd.RustLsp({ "hover", "actions" })
 end, { silent = true, buffer = bufnr, desc = "Hover actions" })
 
 -- Debug
-vim.keymap.set("n", "<leader>dt", function()
+vim.keymap.set("n", "<localleader>da", function()
     vim.cmd.RustLsp("debuggables")
 end, { silent = true, buffer = bufnr, desc = "Debug test" })
-
-vim.keymap.set("n", "<leader>dr", function()
-    vim.cmd.RustLsp("runnables")
-end, { silent = true, buffer = bufnr, desc = "Run" })
 
 -- Tests
 vim.keymap.set("n", "<leader>tt", function()
@@ -33,36 +24,36 @@ vim.keymap.set("n", "gr", function()
     vim.cmd.RustLsp("references")
 end, { silent = true, buffer = bufnr, desc = "Go to references" })
 
-vim.keymap.set("n", "<leader>pm", function()
+vim.keymap.set("n", "<localleader>pm", function()
     vim.cmd.RustLsp("parentModule")
 end, { silent = true, buffer = bufnr, desc = "Go to parent module" })
 
 -- Herramientas
-vim.keymap.set("n", "<leader>rr", function()
+vim.keymap.set("n", "<localleader>rr", function()
     vim.cmd.RustLsp("runnables")
 end, { silent = true, buffer = bufnr, desc = "Run" })
 
-vim.keymap.set("n", "<leader>rem", function()
+vim.keymap.set("n", "<localleader>rem", function()
     vim.cmd.RustLsp("expandMacro")
 end, { silent = true, buffer = bufnr, desc = "Expand macro" })
 
-vim.keymap.set("n", "<leader>rh", function()
+vim.keymap.set("n", "<localleader>rh", function()
     vim.cmd.RustLsp("hover", "actions")
 end, { silent = true, buffer = bufnr, desc = "Hover actions" })
 
-vim.keymap.set("n", "<leader>rj", function()
+vim.keymap.set("n", "<localleader>rj", function()
     vim.cmd.RustLsp("joinLines")
 end, { silent = true, buffer = bufnr, desc = "Join lines" })
 
-vim.keymap.set("n", "<leader>rd", function()
+vim.keymap.set("n", "<localleader>rd", function()
     vim.cmd.RustLsp("renderDiagnostic")
 end, { silent = true, buffer = bufnr, desc = "Render diagnostic" })
 
-vim.keymap.set("n", "<leader>re", function()
+vim.keymap.set("n", "<localleader>ree", function()
     vim.cmd.RustLsp("explainError")
 end, { silent = true, buffer = bufnr, desc = "Explain error" })
 
 -- Documentation
-vim.keymap.set("n", "<leader>doc", function()
+vim.keymap.set("n", "<localleader>roc", function()
     vim.cmd.RustLsp("openDocs")
 end, { silent = true, buffer = bufnr, desc = "Open docs" })
--- lua/plugins/lang/rust/rustaceanvim.lua
+++ lua/plugins/lang/rust/rustaceanvim.lua
@@ -2,37 +2,100 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^6",
     ft = { "rust" },
-    default_settings = {
-        -- rust-analyzer language server configuration
-        ["rust-analyzer"] = {
-            cargo = {
-                allFeatures = true,
+    config = function()
+        vim.g.rustaceanvim = {
+            -- Plugin configuration
+            tools = {
+                hover_actions = {
+                    auto_focus = true,
+                },
+                inlay_hints = {
+                    highlight = "NonText",
+                },
+                on_initialized = function()
+                    vim.lsp.inlay_hint.enable(true)
+                end,
+                test_executor = "background",
             },
-            checkOnSave = true,
-            diagnostics = {
-                enable = true,
-            },
-            procMacro = {
-                enable = true,
-                ignored = {
-                    ["async-trait"] = { "async_trait" },
-                    ["napi-derive"] = { "napi" },
-                    ["async-recursion"] = { "async_recursion" },
+            server = {
+                on_attach = function(client, bufnr)
+                    vim.api.nvim_buf_set_option(bufnr, "formatexpr", "v:lua.vim.lsp.formatexpr()")
+
+                    if client.server_capabilities.documentHighlightProvider then
+                        vim.cmd([[
+                            augroup lsp_document_highlight
+                                autocmd! * <buffer>
+                                autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+                                autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+                            augroup END
+                        ]])
+                    end
+                end,
+                default_settings = {
+                    -- rust-analyzer language server configuration
+                    ["rust-analyzer"] = {
+                        cargo = {
+                            allFeatures = true,
+                            loadOutDirsFromCheck = true,
+                            runBuildScripts = true,
+                        },
+                        -- Habilitar checkOnSave
+                        checkOnSave = true,
+                        check = {
+                            command = "clippy",
+                            extraArgs = { "--all-targets", "--all-features" },
+                        },
+                        diagnostics = {
+                            enable = true,
+                            experimental = {
+                                enable = true,
+                            },
+                        },
+                        -- Habilitar procMacros
+                        procMacro = {
+                            enable = true,
+                            ignored = {
+                                ["async-trait"] = { "async_trait" },
+                                ["napi-derive"] = { "napi" },
+                                ["async-recursion"] = { "async_recursion" },
+                            },
+                        },
+                        -- Configuraciones adicionales
+                        lens = {
+                            enable = true,
+                            debug = true,
+                            implementations = true,
+                            run = true,
+                            methodReferences = true,
+                            references = true,
+                        },
+                        files = {
+                            excludeDirs = {
+                                ".direnv",
+                                ".git",
+                                ".github",
+                                ".gitlab",
+                                "bin",
+                                "node_modules",
+                                "target",
+                                "venv",
+                                ".venv",
+                            },
+                        },
+                        workspace = {
+                            symbol = {
+                                search = {
+                                    scope = "workspace_and_dependencies",
+                                },
+                            },
+                        },
+                    },
                 },
             },
-            files = {
-                excludeDirs = {
-                    ".direnv",
-                    ".git",
-                    ".github",
-                    ".gitlab",
-                    "bin",
-                    "node_modules",
-                    "target",
-                    "venv",
-                    ".venv",
-                },
+            -- DAP configuration
+            dap = {
+                autoload_configurations = true,
             },
-        },
-    },
+        }
+    end,
 }
