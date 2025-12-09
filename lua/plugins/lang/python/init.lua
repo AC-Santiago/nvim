821119aea5a785bf21045884d5e40aad21918486
AC-Santiago <acostasantiago311124578@gmail.com>
2025-12-09 00:00:02 +0000

    Feature: Add uv plugin
    Add the uv plugin to neovim, this is a feature to python language

--- lua/plugins/lang/python/init.lua
+++ lua/plugins/lang/python/init.lua
@@ -1,4 +1,10 @@
 local lang_loader = require("utils.lang_loader")
 local configs = lang_loader.load_lang_configs("python")
+local language_config = require("configs.languages")
 
-return configs
+if language_config.is_enabled("python") then
+    table.insert(configs, { import = "plugins.lang.python.uv-nvim" })
+    return configs
+else
+    return {}
+end
